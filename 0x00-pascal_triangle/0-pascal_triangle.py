ef4054c1f90765af2ea3b8b78a328d6e007f2234
NeMualu <curtis.dior@gmail.com>
2024-10-08 05:25:14 -0700

    commit update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """
 This module contains the pascal_triangle function, which generates
 Pascal's triangle up to a given number of rows.
 """
+
 
 def pascal_triangle(n):
     """
