2de2699e7b6613a696e210190276227d1a59ad47
NeMualu <curtis.dior@gmail.com>
2024-10-08 05:15:04 -0700

    commit update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+This function works as follows:
+
+It checks if n <= 0, and if so, returns an empty list.
+It initializes the triangle with the first row, [1].
+For each subsequent row, it starts and ends with 1. The intermediate elements are calculated by summing the two elements directly above it in the previous row.
+The result is returned as a list of lists representing Pascal's triangle.
+"""
 
 def pascal_triangle(n):
     if n <= 0:
         return []
-
+"""
     # Initialize Pascal's triangle with the first row
-    triangle = [[1]]
+"""
+   triangle = [[1]]
 
     # Build each row of Pascal's triangle
     for i in range(1, n):
